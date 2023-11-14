602f1ecfcc7685a66f97f76632ded33aa1340b31
Lou Fogola <lou.fogola@gmail.com>
2023-11-14 11:23:26 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+import chalk from 'chalk'
+
+const student1 =  "Sarah"
+const student2 = "Baptist"
+const student3 = "Maxence"
+
+console.log(chalk.red(student1))
+console.log(chalk.green(student2))
+console.log(chalk.blue(student3))
